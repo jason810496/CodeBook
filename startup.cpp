b94e5661afe13afd6e86cebc86502c68c952542d
jason810496 <810496@email.wlsh.tyc.edu.tw>
2021-10-05 00:09:58 +0800

    change MEM , add PB

--- startup.cpp
+++ startup.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("O2")
 #include<bits/stdc++.h>
 using namespace std;
 #define OAO cin.tie(0);ios_base::sync_with_stdio(0);
@@ -5,13 +6,13 @@ using namespace std;
 #define S second
 #define ll long long 
 #define range(x) x.begin(),x.end()
-#define MEM(x,i) memset(x,0,sizeof(x))
+#define MEM(x,i) memset(x,i,sizeof(x))
 #define rep(i,n) for(int i=0;i<n;++i)
 #define REP(i,st,n) for(int i=st;i<=n;++i)
+#define PB emplace_back
 typedef pair<int,int> pii;
-const int MAX_N = 1e6+5;
+const int MAX_N = 1e4+5;
 const int INF = 1e9;
-
 /*
 inline bool comp(int a,int b){
     return a>b;
